bc6065bc446a3debdba921ac4fcac0a650c6c9f6
Jack <jack.baker.0299@gmail.com>
2020-01-30 11:15:33 +0000

    Renamed httpRequestConfig to request

--- src/mappers/request.mapper.ts
+++ src/mappers/request.mapper.ts
@@ -2,16 +2,16 @@ import {HttpRequestConfigurations} from '../types/http-configurations.enum';
 import {HttpRequest} from "../types/http-request.class";
 
 export class RequestMapper {
-    public static for(httpRequestConfig: HttpRequest, method: HttpRequestConfigurations): RequestInit {
+    public static for(request: HttpRequest, method: HttpRequestConfigurations): RequestInit {
         return {
-            cache: httpRequestConfig.cache,
-            mode: httpRequestConfig.mode,
-            credentials: httpRequestConfig.credentials,
-            headers: httpRequestConfig.headers,
-            redirect: httpRequestConfig.redirect,
-            referrer: httpRequestConfig.referrer,
+            cache: request.cache,
+            mode: request.mode,
+            credentials: request.credentials,
+            headers: request.headers,
+            redirect: request.redirect,
+            referrer: request.referrer,
             method: method,
-            body: method !== HttpRequestConfigurations.GET ? JSON.stringify(httpRequestConfig.body) : null
+            body: method !== HttpRequestConfigurations.GET ? JSON.stringify(request.body) : null
         };
     }
 }
