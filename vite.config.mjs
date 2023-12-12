492315b58c7ef088d66047c22e78ffeba9dc7815
Jack Baker <jack.baker.0299@gmail.com>
2023-12-12 18:31:39 +0000

    Moving package.json copy to vite build process

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,7 @@
 import {defineConfig} from 'vite';
 import dts from 'vite-plugin-dts';
 import * as path from 'path';
+import {viteStaticCopy} from 'vite-plugin-static-copy';
 
 export default defineConfig({
     build: {
@@ -19,6 +20,14 @@ export default defineConfig({
         }
     },
     plugins: [
-        dts()
+        dts(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: 'package.json',
+                    dest: ''
+                }
+            ]
+        })
     ]
 });
