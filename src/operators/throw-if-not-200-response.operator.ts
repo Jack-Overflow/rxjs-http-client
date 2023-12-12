f564c6c44e9ca68793b78212856870481c48e2c4
Jack Baker <jack.baker.0299@gmail.com>
2023-12-12 18:17:05 +0000

    Refactor to merge map

--- src/operators/throw-if-not-200-response.operator.ts
+++ src/operators/throw-if-not-200-response.operator.ts
@@ -1,10 +1,10 @@
-import {concatMap, Observable, of, OperatorFunction, throwError} from 'rxjs';
+import {mergeMap, Observable, of, OperatorFunction, throwError} from 'rxjs';
 import {HttpResponse} from '../types/http-response.class';
 
 export function throwIfNot200Response(): OperatorFunction<HttpResponse, HttpResponse> {
     return function (source: Observable<HttpResponse>): Observable<HttpResponse> {
         return source.pipe(
-            concatMap((res) => res.ok ? of(res) : throwError(() => res))
+            mergeMap((res) => res.ok ? of(res) : throwError(() => res))
         );
     };
 }
